f19ac6fe99f1a8618109ac571b85a094380b0be1
Michgolden Ukeje <michgoldennathan17@gmail.com>
2024-08-05 14:01:52 +0100

    Remove console log

--- runMockProfileUsersFactory.js
+++ runMockProfileUsersFactory.js
@@ -3,5 +3,4 @@ const path = require("path");
 
 const factoriesDir = path.join(__dirname, "build", "factories");
 const filePath = path.join(factoriesDir, "mockProfiles.factory.js");
-console.log(`running file ${filePath}`);
 require(filePath);
